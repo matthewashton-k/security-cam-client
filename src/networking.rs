7e606bb39b2a3ab8b4e6fc3b38964ba4784dbc2a
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2023-12-16 00:24:26 -0700

    added some tests and implemented login and logout

--- src/networking.rs
+++ src/networking.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::error::Error;
+use reqwest::redirect::Policy;
 use reqwest::Url;
 
 pub struct Client<'a> {
@@ -12,7 +13,7 @@ pub struct Client<'a> {
 impl<'a> Client<'a> {
     pub fn new(addr: &'a str, username: &'a str, password: &'a str) -> Self {
         // doesnt need to be a recoverable error because if it fails then we want our whole program to exit anyways
-        let client_with_cookies = reqwest::Client::builder().cookie_store(true).build().expect("couldn't build http client");
+        let client_with_cookies = reqwest::Client::builder().redirect(Policy::limited(2)).cookie_store(true).build().expect("couldn't build http client");
         Client {
             addr: Url::parse(addr).expect("invalid url"),
             username,
@@ -25,13 +26,24 @@ impl<'a> Client<'a> {
         let mut params = HashMap::new();
         params.insert("username", self.username);
         params.insert("password", self.password);
-        let resp = self.client.post(self.addr).form(&params).send().await?;
-        println!("{:?}", resp.text().await);
+        let resp = self.client.post(self.addr.join("login")?.as_str()).form(&params).send().await?;
+
+        // later on change the webserver to alter the response code if the login fails
+        if !resp.text().await?.contains("Logout") {
+            return Err("login failed".into());
+        }
+
         Ok(())
     }
 
     pub async fn logout(&self) -> Result<(), Box<dyn Error>> {
-        todo!("");
+        let resp = self.client.get(self.addr.join("logout")?.as_str()).send().await?;
+
+        // later on change the webserver to alter the response code if the logout fails
+        if !resp.text().await?.contains("Login") {
+            return Err("logout failed".into());
+        }
+        todo!()
     }
     pub async fn send_and_delete(&self, filename: String) -> Result<(), Box<dyn Error>> {
         todo!("");
@@ -42,10 +54,16 @@ impl<'a> Client<'a> {
 mod tests {
     use security_cam_common::shuttle_runtime::tokio;
 
+    /// only run this test while the server is active
     #[tokio::test]
     async fn test_login() {
         let client = super::Client::new("http://127.0.0.1:8000", "admin", "pass");
         client.login().await.unwrap();
-        client.logout().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_bad_login() {
+        let client = super::Client::new("http://127.0.0.1:8000", "admin", "badpass");
+        client.login().await.unwrap_err();
     }
 }
