e275fb7f0b8aa96ab30d76ef0279cd883b0ffb01
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2023-12-16 09:43:02 -0700

    added encryption stream

--- src/networking.rs
+++ src/networking.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use std::error::Error;
+use nokhwa::query;
 use reqwest::redirect::Policy;
-use reqwest::Url;
+use reqwest::{Body, Url};
 use security_cam_common::encryption::*;
 use security_cam_common::shuttle_runtime::tokio::fs::File;
 
@@ -10,25 +11,17 @@ pub struct Client<'a> {
     username: &'a str,
     password: &'a str,
     client: reqwest::Client,
-    encryptor: EncryptDecrypt,
 }
 
 impl<'a> Client<'a> {
     pub async fn new(addr: &'a str, username: &'a str, password: &'a str) -> Client<'a> {
         // doesnt need to be a recoverable error because if it fails then we want our whole program to exit anyways
         let client_with_cookies = reqwest::Client::builder().redirect(Policy::limited(2)).cookie_store(true).build().expect("couldn't build http client");
-        let (key, salt) = generate_key(password).expect("couldnt generate keystream");
-        let encryptor = EncryptDecrypt {
-            key: Some(key),
-            salt: Some(salt),
-            file: File::create("first.mp4").await.expect("couldn't create first mp4 file")
-        };
         Client {
             addr: Url::parse(addr).expect("invalid url"),
             username,
             password,
             client:  client_with_cookies,
-            encryptor
         }
     }
 
@@ -53,10 +46,21 @@ impl<'a> Client<'a> {
         if !resp.text().await?.contains("Login") {
             return Err("logout failed".into());
         }
-        todo!()
+        Ok(())
     }
     pub async fn send_and_delete(&self, filename: String) -> Result<(), Box<dyn Error>> {
-        todo!("");
+        let (key, salt) = generate_key(self.password).expect("couldnt generate keystream");
+
+        // creating a new encryptor each time we need to send a file is less than optimal, but it will have to do until I change the common api
+        let encryptor = EncryptDecrypt {
+            key: Some(key),
+            salt: Some(salt),
+            file: File::options().read(true).write(true).open(filename).await?
+        };
+        let stream = Box::pin(encryptor.encrypt_stream());
+        let resp = self.client.post(self.addr.join("new_video")?.as_str()).body(Body::wrap_stream(stream)).send().await?;
+        println!("status: {:?}, text: {:?}",resp.status(), resp.text().await, );
+        Ok(())
     }
 }
 
@@ -75,5 +79,19 @@ mod tests {
     async fn test_bad_login() {
         let client = super::Client::new("http://127.0.0.1:8000", "admin", "badpass").await;
         client.login().await.unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn test_logout() {
+        let client = super::Client::new("http://127.0.0.1:8000", "admin", "pass").await;
+        client.login().await.unwrap();
+        client.logout().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_send_video() {
+        let client = super::Client::new("http://127.0.0.1:8000", "admin", "pass").await;
+        client.login().await.unwrap();
+        client.send_and_delete("test.mp4".to_string()).await.unwrap();
     }
 }
